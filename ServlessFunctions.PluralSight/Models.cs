ae1185cb89a492d308bf8f00cd60c8072c1d0f08
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2022-07-12 19:22:15 -0300

    feat: choosing a database

--- ServlessFunctions.PluralSight/Models.cs
+++ ServlessFunctions.PluralSight/Models.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace ServlessFunctions.PluralSight;
 
@@ -19,4 +20,37 @@ public class TodoUpdateModel
 {
     public string TaskDescription { get; set; }
     public bool IsCompleted { get; set; }
+}
+
+public class TodoTableEntity : TableEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public string TaskDescription { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+public static class Mappings
+{
+    public static TodoTableEntity ToTableEntity(this Todo todo)
+    {
+        return new TodoTableEntity
+        {
+            PartitionKey = "TODO",
+            RowKey = todo.Id,
+            CreatedAt = todo.CreatedAt,
+            IsCompleted = todo.IsCompleted,
+            TaskDescription = todo.TaskDescription
+        };
+    }
+
+    public static Todo ToTodo(this TodoTableEntity todo)
+    {
+        return new Todo
+        {
+            Id = todo.RowKey,
+            CreatedAt = todo.CreatedAt,
+            IsCompleted = todo.IsCompleted,
+            TaskDescription = todo.TaskDescription
+        };
+    }
 }
