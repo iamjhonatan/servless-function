317c413996a8c88a036978da3b97f7179d63a0d3
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2022-07-11 23:05:31 -0300

    feat: set parameters and add configurations of CORS

--- ServlessFunctions.PluralSight/Models.cs
+++ ServlessFunctions.PluralSight/Models.cs
@@ -5,7 +5,7 @@ namespace ServlessFunctions.PluralSight;
 public class Todo
 {
     public string Id { get; set; } = Guid.NewGuid().ToString("n");
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
     public string TaskDescription { get; set; }
     public bool IsCompleted { get; set; }
 }
